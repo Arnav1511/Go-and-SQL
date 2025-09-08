42531912320e6bbc564c53d4c5b86cc3f1e6e95a
Arnav1511 <meetarnavranjan@gmail.com>
2025-09-09 02:53:48 +0530

    adding config file

--- go-bookstore/pkg/config/apps.go
+++ go-bookstore/pkg/config/apps.go
@@ -1,1 +1,19 @@
 package config
+
+import "github.com/jinzhu/gorm"
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "arnav:passasd123;??charset=utf8&parseTIme=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
